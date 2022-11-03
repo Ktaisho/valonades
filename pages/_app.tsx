ef952c0a6e078c6a61f1e111078c6edd075b9943
sakho13 <wecl.khotaro@gmail.com>
2022-11-03 20:41:39 +0900

    heroicons 追加

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,9 @@
 import "../styles/globals.css"
+import { SunIcon, MoonIcon } from "@heroicons/react/24/solid"
 import type { AppProps } from "next/app"
 import Head from "next/head"
 import { useState } from "react"
+import styles from "@/styles/App.module.scss"
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [isDarkMode, setIsDarkMode] = useState<boolean>(false)
@@ -12,7 +14,7 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <div
-      className={`${isDarkMode ? "dark" : ""} ${
+      className={`${isDarkMode ? "dark" : "light"} ${
         !isDarkMode ? "bg-white" : "bg-black text-white"
       }`}
       style={{ minHeight: "100vh" }}
@@ -26,37 +28,18 @@ function MyApp({ Component, pageProps }: AppProps) {
       <header style={{ display: "flex", justifyContent: "space-evenly" }}>
         <h1 className={`font-bold text-2xl`}>ここはヘッダー</h1>
 
-        <button onClick={toggleDarkMode}>
-          {!isDarkMode ? (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
-              />
-            </svg>
+        <button
+          onClick={toggleDarkMode}
+          className={
+            styles.DarkModeToggleButton +
+            ` hover:text-white hover:bg-black` +
+            ` dark:hover:bg-white dark:hover:text-black`
+          }
+        >
+          {isDarkMode ? (
+            <SunIcon className="w-[80%] h-[80%] align-none inline" />
           ) : (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
-              />
-            </svg>
+            <MoonIcon className="w-[80%] h-[80%] align-none inline" />
           )}
         </button>
       </header>
---
+++ pages/maps/[id].tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next"
+
+const MapDetail: NextPage = () => {
+  return (
+    <div className={``}>
+      <h2 className="text-2xl font-bold">これが見れたら成功</h2>
+    </div>
+  )
+}
+
+export default MapDetail
