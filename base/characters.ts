f8c592732c2357dd587c17c60e464f5b4668c4ee
hiroto <hiroto.k.0203@gmail.com>
2022-11-03 22:34:13 +0900

    定数作成。キャラとマップ全部

--- base/characters.ts
+++ base/characters.ts
@@ -1,8 +1,130 @@
-export const CharacterIds = ["a", "b"] as const
-
 export type CharacterType = {
-  id: typeof CharacterIds[number]
+  id: string
   name: string
+  role: string
+  release: number
 }
 
-export const Characters: CharacterType[] = []
+export const Characters: CharacterType[] = [
+  // beta = 0.0
+  {
+    id: "Astra",
+    name: "アストラ",
+    role: "Controller",
+    release: 2.04,
+  },
+  {
+    id: "Breach",
+    name: "ブリーチ",
+    role: "Initiator",
+    release: 0.0,
+  },
+  {
+    id: "Brimstone",
+    name: "ブリムストーン",
+    role: "Controller",
+    release: 0.0,
+  },
+  {
+    id: "Chamber",
+    name: "チェンバー",
+    role: "Sentinel",
+    release: 3.1,
+  },
+  {
+    id: "Cypher",
+    name: "サイファー",
+    role: "Sentinel",
+    release: 0.0,
+  },
+  {
+    id: "Fade",
+    name: "フェイド",
+    role: "Initiator",
+    release: 4.08,
+  },
+  {
+    id: "Harbor",
+    name: "ハーバー",
+    role: "controller",
+    release: 5.08,
+  },
+  {
+    id: "Jett",
+    name: "ジェット",
+    role: "Duelist",
+    release: 0.0,
+  },
+  {
+    id: "Kay/O",
+    name: "ケイオー",
+    role: "Initiator",
+    release: 3.0,
+  },
+  {
+    id: "Killjoy",
+    name: "キルジョイ",
+    role: "Sentinel",
+    release: 1.05,
+  },
+  {
+    id: "Neon",
+    name: "ネオン",
+    role: "Duelist",
+    release: 4.0,
+  },
+  {
+    id: "Omen",
+    name: "オーメン",
+    role: "Controller",
+    release: 0.0,
+  },
+  {
+    id: "Phoenix",
+    name: "フェニックス",
+    role: "Duelist",
+    release: 0.0,
+  },
+  {
+    id: "Raze",
+    name: "レイズ",
+    role: "Duelist",
+    release: 0.0,
+  },
+  {
+    id: "Reyna",
+    name: "レイナ",
+    role: "Duelist",
+    release: 1.0,
+  },
+  {
+    id: "Sage",
+    name: "セージ",
+    role: "Sentinel",
+    release: 0.0,
+  },
+  {
+    id: "Skye",
+    name: "セージ",
+    role: "Initiator",
+    release: 1.11,
+  },
+  {
+    id: "Sova",
+    name: "ソーヴァ",
+    role: "Initiator",
+    release: 0.0,
+  },
+  {
+    id: "Viper",
+    name: "バイパー",
+    role: "Controller",
+    release: 0.0,
+  },
+  {
+    id: "Yoru",
+    name: "ヨル",
+    role: "Duelist",
+    release: 2.0,
+  },
+]
