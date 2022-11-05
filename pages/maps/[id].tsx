9ec11c90c920910b0047e78775fb111ad5043941
sakho13 <56495487+sakho13@users.noreply.github.com>
2022-11-05 16:57:04 +0900

    追加: マップページ大体の機能

--- pages/maps/[id].tsx
+++ pages/maps/[id].tsx
@@ -1,18 +1,31 @@
 import type { NextPage } from "next"
 import { useRouter } from "next/router"
 import { useEffect, useState } from "react"
-import { MapIds } from "@/base/maps"
+import { Characters, CharacterType } from "@/base/characters"
+import { Maps } from "@/base/maps"
 
 const MapDetail: NextPage = () => {
   const router = useRouter()
 
-  const [mapId, setMapId] = useState<typeof MapIds[number] | null>(null)
+  const [mapId, setMapId] = useState<string>("")
+  const [selectedCharacter, setCharcter] = useState<CharacterType | null>(null)
 
   const fetchData = () => {
     const { id } = router.query
 
-    if (typeof id == "string" && MapIds.includes(id)) {
-      setMapId(id)
+    const mapid = Maps.find((map) => map.id === id)
+
+    if (mapid) {
+      setMapId(mapid.id)
+    }
+  }
+
+  const selectCharacter = (characterId: string) => {
+    const character = Characters.find((c) => c.id === characterId)
+    if (character === undefined) {
+      console.log("You selected an undefined character.")
+    } else {
+      setCharcter(character)
     }
   }
 
@@ -21,8 +34,58 @@ const MapDetail: NextPage = () => {
   }, [router.query])
 
   return (
-    <div className={``}>
-      <h2 className="text-2xl font-bold">Map ID: {mapId}</h2>
+    <div className={[`w-[100%]`, `flex`, `flex-col md:flex-row`].join(" ")}>
+      {/* <h2 className="text-2xl font-bold">Map ID: {mapId}</h2> */}
+      <div className={[`md:order-1`].join(" ")}>
+        <p>Abilities </p>
+        <div>
+          {selectedCharacter ? (
+            Object.entries(selectedCharacter.skills).map(([n, ability]) => {
+              return <div key={n}>{ability.name}</div>
+            })
+          ) : (
+            <div></div>
+          )}
+        </div>
+      </div>
+
+      <div
+        className={[
+          `min-w-[800px]`,
+          `h-[600px]`,
+          `md:mr-[2px]`,
+          `order-1 md:order-2`,
+        ].join(" ")}
+      >
+        {/*  */}
+      </div>
+
+      <div className={[`md:order-3`].join(" ")}>
+        <p>Characters</p>
+        <div className={[`w-[100%]`, `flex`, `flex-wrap`].join(" ")}>
+          {
+            // キャラ選択リスト
+            Characters.map((character) => {
+              return (
+                <div
+                  key={character.id}
+                  className={[
+                    `w-[40px]`,
+                    `h-[40px]`,
+                    `bg-gray-300`,
+                    `rounded-[50%]`,
+                    `cursor-pointer`,
+                  ].join(" ")}
+                  onClick={() => selectCharacter(character.id)}
+                >
+                  {character.name[0]}
+                  {/* {character.name} */}
+                </div>
+              )
+            })
+          }
+        </div>
+      </div>
     </div>
   )
 }
