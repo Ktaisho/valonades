ff46f97e466c4a0611c53406ab02ef50ac94c011
sakho13 <wecl.khotaro@gmail.com>
2023-01-09 18:50:21 +0900

    追加: MapComponentのベース

--- components/MapCompoenet.tsx
+++ components/MapCompoenet.tsx
@@ -1,1 +1,11 @@
-// test
+import classNames from "classnames"
+
+const MapComponent: React.FC = () => {
+  return (
+    <div className={classNames([`w-[inherit]`, `h-[inherit]`])}>
+      <h1>MAP</h1>
+    </div>
+  )
+}
+
+export default MapComponent
